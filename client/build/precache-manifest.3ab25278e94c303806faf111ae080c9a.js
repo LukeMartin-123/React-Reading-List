06946b1a80d4d206a6f187c219556f980daa8fc0
Luke Martin <ljmart02@gmail.com>
2021-02-10 09:24:59 -0700

    deploy

---
+++ client/build/precache-manifest.3ab25278e94c303806faf111ae080c9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abdb4cf2e8cdbb7112dca910f296d08d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01299706730da037f3e6",
+    "url": "/static/css/main.997d4878.chunk.css"
+  },
+  {
+    "revision": "5be6bd2e49deeef50f9b",
+    "url": "/static/js/2.e411f8c6.chunk.js"
+  },
+  {
+    "revision": "223be75f6809993b56a94be00d710bed",
+    "url": "/static/js/2.e411f8c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01299706730da037f3e6",
+    "url": "/static/js/main.11fffd9b.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  }
+]);
